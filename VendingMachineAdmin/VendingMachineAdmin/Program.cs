67eab06f30387df1b8f434eefdb291ccc2f83d4f
Claudia Pop <popclaudia97@gmail.com>
2019-04-17 23:07:33 +0300

    Initial Admin Solution

---
+++ VendingMachineAdmin/VendingMachineAdmin/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace VendingMachineAdmin
+{
+    class Program
+    {
+
+        public static int Main(String[] args)
+        {
+            AdminSocket.StartListening("Admin sent this as result");
+            return 0;
+        }
+    }
+}
---
+++ VendingMachineAdmin/VendingMachineAdmin/UI.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace VendingMachineAdmin
+{
+    class UI
+    {
+
+    }
+}
