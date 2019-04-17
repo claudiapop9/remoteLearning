e972507d7cc7628c4a5f4bda774e0aadf1cc95e7
Claudia Pop <popclaudia97@gmail.com>
2019-04-17 23:25:18 +0300

    Admin: logging added

--- VendingMachineAdmin/VendingMachineAdmin/Program.cs
+++ VendingMachineAdmin/VendingMachineAdmin/Program.cs
@@ -8,10 +8,12 @@ namespace VendingMachineAdmin
 {
     class Program
     {
-
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static int Main(String[] args)
         {
+
             AdminSocket.StartListening("Admin sent this as result");
+            log.Info("Admin Log");
             return 0;
         }
     }
